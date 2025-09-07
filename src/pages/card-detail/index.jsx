83ee928965445b02fdbd6958d98704a3fac53f8c
Thien Truong <xayda.ln@gmail.com>
2025-09-07 16:01:26 +0700

    Display Card Detail Added

--- src/pages/card-detail/index.jsx
+++ src/pages/card-detail/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./index.scss";
 import Home from '../home';
 import FrontCardDetail from '../../components/front-card-detail';
@@ -6,17 +6,57 @@ import BehindCardDetail from '../../components/behind-card-detail copy';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function CardDetail() {
+
+  const [cardDetail, setCardDetail] = useState([]);
+
+  const fetchCardDetail = async () => {
+    const response = await axios.get('https://68ad6481a0b85b2f2cf324aa.mockapi.io/Users');
+    const lastItem = response.data[response.data.length -1];
+    setCardDetail(lastItem);
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchCardDetail();
+  }, [])
+
   return (
     <div className='card-detail'>
       <div className="card">
-        <div className="front-card">
-          <FrontCardDetail />
+        <div className='front-card-detail'>
+          <div className="logo">
+            <div className='left-circle'></div>
+            <div className='right-circle'></div>
+          </div>
+
+          <div className="card-number">
+            <span>{cardDetail.cardHolderNumber || '0000 0000 0000 0000'}</span>
+          </div>
+
+          <div className="info">
+            <div className="card-owner">
+              <span>{cardDetail.cardHolderName || 'Michael Jackson'}</span>
+            </div>
+
+            <div className="card-date">
+              <span>{(cardDetail.month || '00') + '/' + (cardDetail.year || '00')}</span>
+            </div>
+          </div>
         </div>
 
-        <div className="behind-card">
-          <BehindCardDetail />
+        <div className='behind-card-detail'>
+          <div className="line"></div>
+          <div className="card-cvc">
+            <span>{cardDetail.cvc || "000"}</span>
+          </div>
+          <div className='under'>
+            <hr />
+            <hr />
+            <hr />
+          </div>
         </div>
       </div>
 
